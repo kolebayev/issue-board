28269eede7396954b8a3553027e789abc9c58fab
Ilya Kolebayev <kolebayev@gmail.com>
2021-02-21 16:27:36 +0300

    Add header logo

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import './Header.scss';
+import React from 'react'
+import './Header.scss'
 import { Text } from '@consta/uikit/Text'
 import { Button } from '@consta/uikit/Button'
-import { useStoreActions, Actions, useStoreState, State } from 'easy-peasy';
+import { useStoreActions, Actions, useStoreState, State } from 'easy-peasy'
 import { ModelTypes } from '../../store/store'
+import logo from '../../images/logo.svg'
+import logo20 from '../../images/logo20.svg'
+import logo30 from '../../images/logo30.svg'
 
-type StoreActions = Actions<ModelTypes>;
+type StoreActions = Actions<ModelTypes>
 type StoreState = State<ModelTypes>
 
 function Header() {
-  const setSidebarSettingsIsOpen = useStoreActions((actions: StoreActions) => actions.setSidebarSettingsIsOpen)
-  const sidebarSettingsIsOpen = useStoreState((state: StoreState) => state.sidebarSettingsIsOpen);
+  const setSidebarSettingsIsOpen = useStoreActions(
+    (actions: StoreActions) => actions.setSidebarSettingsIsOpen
+  )
+  const sidebarSettingsIsOpen = useStoreState(
+    (state: StoreState) => state.sidebarSettingsIsOpen
+  )
   return (
     <div className="Header">
-      <Text view="primary" size='m'>CONSTALOGO | IssueDeck</Text>
-      <Button view='ghost' label='А це шо?' size='xs' onClick={() => setSidebarSettingsIsOpen(!sidebarSettingsIsOpen)} />
+      <div className="Header_leftSide">
+        <img src={logo30} alt="Consta logo" className="Header_leftSide_logo" />
+        <Text view="primary" size="m">
+          IssueDeck
+        </Text>
+      </div>
+      <Button
+        view="ghost"
+        label="А це шо?"
+        size="xs"
+        onClick={() => setSidebarSettingsIsOpen(!sidebarSettingsIsOpen)}
+      />
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
--- src/Components/SidebarSettings/SidebarSettings.tsx
+++ src/Components/SidebarSettings/SidebarSettings.tsx
@@ -6,36 +6,50 @@ import { Informer } from '@consta/uikit/Informer'
 import { Button } from '@consta/uikit/Button'
 import { IconClose } from '@consta/uikit/IconClose'
 import InputWithLabel from '../InputWithLabel/InputWithLabel'
-import { useStoreActions, Actions, useStoreState, State } from 'easy-peasy';
+import { useStoreActions, Actions, useStoreState, State } from 'easy-peasy'
 import { ModelTypes } from '../../store/store'
 import { request } from '@octokit/request'
 
-type StoreActions = Actions<ModelTypes>;
+type StoreActions = Actions<ModelTypes>
 type StoreState = State<ModelTypes>
 
 function SidebarSettings() {
-  const setSidebarSettingsIsOpen = useStoreActions((actions: StoreActions) => actions.setSidebarSettingsIsOpen)
-  const sidebarSettingsIsOpen = useStoreState((state: StoreState) => state.sidebarSettingsIsOpen);
+  const setSidebarSettingsIsOpen = useStoreActions(
+    (actions: StoreActions) => actions.setSidebarSettingsIsOpen
+  )
+  const sidebarSettingsIsOpen = useStoreState(
+    (state: StoreState) => state.sidebarSettingsIsOpen
+  )
   const closeSidebar = () => setSidebarSettingsIsOpen(!sidebarSettingsIsOpen)
 
-  const repoOwner = useStoreState((state: StoreState) => state.repoOwner);
-  const setRepoOwner = useStoreActions((actions: StoreActions) => actions.setRepoOwner)
+  const repoOwner = useStoreState((state: StoreState) => state.repoOwner)
+  const setRepoOwner = useStoreActions(
+    (actions: StoreActions) => actions.setRepoOwner
+  )
 
-  const repoName = useStoreState((state: StoreState) => state.repoName);
-  const setRepoName = useStoreActions((actions: StoreActions) => actions.setRepoName)
+  const repoName = useStoreState((state: StoreState) => state.repoName)
+  const setRepoName = useStoreActions(
+    (actions: StoreActions) => actions.setRepoName
+  )
 
-  const githubToken = useStoreState((state: StoreState) => state.githubToken);
-  const setGithubToken = useStoreActions((actions: StoreActions) => actions.setGithubToken)
+  const githubToken = useStoreState((state: StoreState) => state.githubToken)
+  const setGithubToken = useStoreActions(
+    (actions: StoreActions) => actions.setGithubToken
+  )
 
-  const fetchIssues = async (githubToken: string, repoOwner: string, repoName: string) => {
-    return await request("GET /repos/{owner}/{repo}/issues", {
+  const fetchIssues = async (
+    githubToken: string,
+    repoOwner: string,
+    repoName: string
+  ) => {
+    return await request('GET /repos/{owner}/{repo}/issues', {
       headers: {
         authorization: githubToken as string,
       },
       owner: repoOwner as string,
       repo: repoName as string,
-      per_page: 100
-    });
+      per_page: 100,
+    })
   }
 
   const inputGroup = [
@@ -59,29 +73,48 @@ function SidebarSettings() {
     <Sidebar
       isOpen={sidebarSettingsIsOpen}
       hasOverlay
-      position='right'
-      className='SidebarSettings'
+      position="right"
+      className="SidebarSettings"
       onClickOutside={closeSidebar}
     >
-      <div className='SidebarSettings_content'>
+      <div className="SidebarSettings_content">
         <div>
-          <div className='SidebarSettings_mainHeading'>
-            <Text view='primary' weight='bold' size='3xl'>
+          <div className="SidebarSettings_mainHeading">
+            <Text view="primary" weight="bold" size="3xl">
               IssueDeck
             </Text>
-            <Button view='clear' iconLeft={IconClose} onlyIcon size='s' onClick={closeSidebar} />
+            <Button
+              view="clear"
+              iconLeft={IconClose}
+              onlyIcon
+              size="s"
+              onClick={closeSidebar}
+            />
           </div>
-          <Text view='secondary' size='l' className='SidebarSettings_introParagraph'>
-            IssueDeck — утилита для просмотра issues в открытых репозиториях GitHub.
-            По умолчанию подключен репозиторий дизайн-системы
-            <Text view='link' size='l' as='a' href=''> Consta</Text>.
+          <Text
+            view="secondary"
+            size="l"
+            className="SidebarSettings_introParagraph"
+          >
+            IssueDeck — утилита для просмотра issues в открытых репозиториях
+            GitHub. По умолчанию подключен репозиторий дизайн-системы
+            <Text view="link" size="l" as="a" href="">
+              {' '}
+              Consta
+            </Text>
+            .
           </Text>
-          <Informer label="Для работы IssueDeck вам понадобится токен GitHub" view="filled" status="system" className='SidebarSettings_informer' />
+          <Informer
+            label="Для работы IssueDeck вам понадобится токен GitHub"
+            view="filled"
+            status="system"
+            className="SidebarSettings_informer"
+          />
           <InputWithLabel
             labelLeft={inputGroup[0].labelLeft}
             labelRight={inputGroup[0].labelRight}
             placeholder={inputGroup[0].placeholder}
-            width='full'
+            width="full"
             defaultValue={repoOwner}
             onInputChange={setRepoOwner}
           />
@@ -89,7 +122,7 @@ function SidebarSettings() {
             labelLeft={inputGroup[1].labelLeft}
             labelRight={inputGroup[1].labelRight}
             placeholder={inputGroup[1].placeholder}
-            width='full'
+            width="full"
             defaultValue={repoName}
             onInputChange={setRepoName}
           />
@@ -97,28 +130,29 @@ function SidebarSettings() {
             labelLeft={inputGroup[2].labelLeft}
             labelRight={inputGroup[2].labelRight}
             placeholder={inputGroup[2].placeholder}
-            width='full'
+            width="full"
             defaultValue={githubToken}
             onInputChange={setGithubToken}
           />
         </div>
         <Button
-          view='primary'
-          size='l'
-          width='full'
-          label='Загрузить issues'
-          className='SidebarSettings_ctaButton'
+          view="primary"
+          size="l"
+          width="full"
+          label="Загрузить issues"
+          className="SidebarSettings_ctaButton"
           onClick={() => {
             if (githubToken && repoOwner && repoName) {
-              fetchIssues(githubToken, repoOwner, repoName).then(data => console.log(data))
+              fetchIssues(githubToken, repoOwner, repoName).then((data) =>
+                console.log(data)
+              )
             } else {
-              alert('Не указан один из параметров запроса');
+              alert('Не указан один из параметров запроса')
             }
           }}
         />
       </div>
     </Sidebar>
-
   )
 }
 
